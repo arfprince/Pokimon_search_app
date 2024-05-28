b046f616f23afbe106a9685af5d0d199a43eec8d
ARFprince <parmanurrahmanfaisal@gmail.com>
2024-05-29 03:08:55 +0600

    "almost"

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ const pokemonId=document.querySelector("#pokemon-id");
 const pokemonHeight=document.querySelector("#height");
 const pokemonWeight=document.querySelector("#weight");
 const pokemonHP=document.querySelector("#hp");
-const pokemonTypes=document.querySelector("#types");
 const pokemonAttack=document.querySelector("#attack");
 const pokemonDefense=document.querySelector("#defense");
 const specialAttack=document.querySelector("#special-attack");
 const specialDefense=document.querySelector("#special-defense");
-const speed=document.querySelector("#speed");
-
+const pokemonSpeed=document.querySelector("#speed");
+const pokemonPowerTypes=document.querySelector("#types");
+const pokemonPic=document.querySelector("#pic");
 
 async function getData(poki) {
     try {
@@ -24,15 +24,51 @@ async function getData(poki) {
             return response.json();
         }
     } catch (error) {
+        alert("Pokémon not found");
         console.error();
+    }
+}
+function randerPokimonInfo(data){
+    pokemonName.innerText=data.name.toUpperCase();
+    pokemonId.innerText=`#${data.id}`;
+    pokemonWeight.innerText=`Weight: ${data.weight}`;
+    pokemonHeight.innerText=`Height: ${data.height}`;
+    for(let i=0;i<data.types.length;i++){
+        let span=document.createElement("span");
+        span.innerHTML=`<div style="height: 25px; width: 80px; background-color: crimson; border-radius: 5px; margin: 4px; padding: 4px;"> ${data.types[i].type.name} </div>`;
+        pokemonPowerTypes.appendChild(span);
+    }
+
+    pokemonPic.src=data.sprites.front_default;
+
+    for(let i=0;i<data.stats.length;i++){
+        if(data.stats[i].stat.name==="hp"){
+            pokemonHP.innerText=data.stats[i].base_stat;
+        }
+        else if(data.stats[i].stat.name==="attack"){
+            pokemonAttack.innerText=data.stats[i].base_stat;
+        }
+        else if(data.stats[i].stat.name==="defense"){
+            pokemonDefense.innerText=data.stats[i].base_stat;
+        }
+        else if(data.stats[i].stat.name==="special-defense"){
+            specialDefense.innerText=data.stats[i].base_stat;
+        }
+        else if(data.stats[i].stat.name==="special-attack"){
+            specialAttack.innerText=data.stats[i].base_stat;
+        }
+        else if(data.stats[i].stat.name==="speed"){
+            pokemonSpeed.innerText=data.stats[i].base_stat;
+        }
     }
 }
 
 button.addEventListener("click", async ()=>{
+    pokemonPowerTypes.innerHTML="";
     let poki=input.value;
     if(poki){
         const data = await getData(poki);
-        console.log(data);
+        randerPokimonInfo(data);
     }
     else{
         alert("You must give a Name or Id");
